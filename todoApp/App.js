e8d66d51b09aad15e6b2510ae95367b3f80d9e31
gimyeong <dlrlaud0806@gmail.com>
2022-11-08 22:05:29 +0900

    todoApp

---
+++ todoApp/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	TouchableHighlight,
+} from "react-native";
+import { theme } from "./colors";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<View style={styles.header}>
+				<TouchableOpacity>
+					<Text style={styles.btnText}>Work</Text>
+				</TouchableOpacity>
+				<TouchableOpacity>
+					<Text style={styles.btnText}>Travel</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: theme.bg,
+		paddingHorizontal: 20,
+	},
+	header: {
+		justifyContent: "space-between",
+		flexDirection: "row",
+		marginTop: 100,
+	},
+	btnText: {
+		fontSize: 38,
+		fontWeight: "600",
+		color: "white",
+	},
+});
